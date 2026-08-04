c910567f4023bb0e9edf428c2545e1431cc30971
maqaqadi <Linda.Masondo@universal.co.za>
2026-08-04 21:16:33 +0200

    fix: update DFS rewrite to new production URL

--- next.config.js
+++ next.config.js
@@ -76,11 +76,11 @@ const nextConfig = {
     return [
       {
         source: '/digital-footprint',
-        destination: 'https://digital-footprint-scanner-7ws8y12oh-linda-masondos-projects.vercel.app/',
+        destination: 'https://digital-footprint-scanner-49k0ve8q2-linda-masondos-projects.vercel.app/',
       },
       {
         source: '/digital-footprint/:path*',
-        destination: 'https://digital-footprint-scanner-7ws8y12oh-linda-masondos-projects.vercel.app/:path*',
+        destination: 'https://digital-footprint-scanner-49k0ve8q2-linda-masondos-projects.vercel.app/:path*',
       },
       {
         source: '/trust-profile',
